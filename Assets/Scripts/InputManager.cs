a96c7788a1c0861250350542bf6347fcdf493feb
Randy D. Epouhe <repouhe@andrew.cmu.edu>
2019-02-06 22:21:21 -0500

    Added movement for multiple small players.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 using XboxCtrlrInput;
 
 public class InputManager : MonoBehaviour {
-  [SerializeField] private SmallPlayer smallPlayer1;
-  //[SerializeField] private SmallPlayer smallPlayer2;
-  //[SerializeField] private SmallPlayer smallPlayer3;
+    //[SerializeField] private SmallPlayer smallPlayer1;
+    //[SerializeField] private SmallPlayer smallPlayer2;
+    //[SerializeField] private SmallPlayer smallPlayer3;
+    //[SerializeField] private SmallPlayer smallPlayer4;
+  public SmallPlayer[] players = new SmallPlayer[4];
+  private XboxController[] controllers = new XboxController[] { XboxController.First, XboxController.Second, XboxController.Third, XboxController.Fourth };
 
   private void Update() {
-    float xMove = XCI.GetAxis(XboxAxis.LeftStickX);
-    float zMove = XCI.GetAxis(XboxAxis.LeftStickY);
-    smallPlayer1.Move(xMove, zMove);
 
-    float Xrotation = XCI.GetAxis(XboxAxis.RightStickX);
-    float Zrotation = XCI.GetAxis(XboxAxis.RightStickY);
-    if (!(Xrotation == 0 && Zrotation == 0)) {
-      smallPlayer1.SetRotation(Xrotation, Zrotation);
+    for (int i = 0; i < 2; i++) //For four controllers connected to the machine.
+    {
+       checkInputs(controllers[i], players[i]);
     }
   }
+  
+  //Checks for inputs from a specific controller, and applies movement to the s
+  private void checkInputs(XboxController controller, IXzController player)
+  {
+
+        float xMove = XCI.GetAxis(XboxAxis.LeftStickX,controller);
+        float zMove = XCI.GetAxis(XboxAxis.LeftStickY,controller);
+        player.Move(xMove, zMove);
+
+        float Xrotation = XCI.GetAxis(XboxAxis.RightStickX,controller);
+        float Zrotation = XCI.GetAxis(XboxAxis.RightStickY,controller);
+        if (!(Xrotation == 0 && Zrotation == 0))
+        {
+            player.SetRotation(Xrotation, Zrotation);
+        }
+    }
 }
