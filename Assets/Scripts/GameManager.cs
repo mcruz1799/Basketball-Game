5615271b924547b557266804d5766c554c6e8b57
Kalpa <kanjur@andrew.cmu.edu>
2019-02-07 16:11:11 -0500

    updated gm more

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using XboxCtrlrInput;
 
 public class GameManager : MonoBehaviour {
+    public XboxController controller;
+    public GameObject ball;
+    public Transform player1;
+    public Transform player2;
+    public Transform player3;
+    public Transform player4;
     static int score_team1;
     static int score_team2;
     [SerializeField] int game_time_ins;
@@ -42,15 +49,14 @@ public class GameManager : MonoBehaviour {
     }
 
     static void start_game(){
-        setGameTime(game_time);
-        gm.StartCoroutine(GameTime());
-        gm.StartCoroutine(UpdateTime());
+        gm.StartCoroutine(TipOff());
         score_team1 = 0;
         score_team2 = 0;
         overtime = false;
         game_time = gm.game_time_ins;
         winning_score = gm.winning_score_ins;
         curr_time = game_time;
+        setGameTime(curr_time);
     }
 
     static void end_game(){
@@ -124,6 +130,31 @@ public class GameManager : MonoBehaviour {
             yield return new WaitForSeconds(1);
             setGameTime(curr_time);
         }
+    }
+
+    IEnumerator TipOff(){
+        while (true){
+            if (XCI.GetButtonDown(XboxButton.A)){
+                switch(controller)
+                {
+                    case XboxController.First:
+                        ball.GetComponent<Ball>().SetParent(player1);
+                        break;
+                    case XboxController.Second:
+                        ball.GetComponent<Ball>().SetParent(player2);
+                        break;
+                    case XboxController.Third:
+                        ball.GetComponent<Ball>().SetParent(player3);
+                        break;
+                    case XboxController.Fourth:
+                        ball.GetComponent<Ball>().SetParent(player4);
+                        break;
+                }
+            }
+        }
+        gm.StartCoroutine(GameTime()); //start counting the game
+        gm.StartCoroutine(UpdateTime()); //start updating the game
+        yield return new WaitForSeconds(0);
     }
 
     private static void setGameTime(float time)
