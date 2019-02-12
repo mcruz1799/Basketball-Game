da30a8a59c358fcabee5752f7481f7539941078a
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-12 14:37:55 -0500

    Clean up PlayerMover a little

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -11,28 +11,17 @@ public class PlayerMover : MonoBehaviour, IXzController {
 
   public float XLook { get { return transform.forward.x; } }
   public float ZLook { get { return transform.forward.z; } }
-  //private bool grounded = true;
-    // Update is called once per frame
     
 
   public void Move(float xMove, float zMove) {
     Vector3 movementVector = new Vector3(xMove, 0, zMove);
+
+    //Moves on world axis, deltatime to smoothen movement
     transform.Translate(movementVector.normalized * Time.deltaTime * movementSpeed, Space.World);
-    //moves on world axis, deltatime to smoothen movement ^^
   }
 
   public void SetRotation(float xLook, float zLook) {
     Vector3 target = new Vector3(xLook, 0, zLook);
     transform.forward = target;
-  }
-
-  public void PressA(XboxController controller)
-  {
-        //Added to remove compile-time issues.
-  }
-  
-  public void PressB(XboxController controller)
-  {
-
   }
 }
