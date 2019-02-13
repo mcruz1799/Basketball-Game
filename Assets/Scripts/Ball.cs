14b5cc65c53ce473901fb617de3b7ce3602f7143
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-13 11:24:38 -0500

    Add gfx to Ball.SetPosition()

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour, IBall {
+  [SerializeField] private DigitalRuby.LightningBolt.LightningBoltScript lightningEffectPrefab;
+  [SerializeField] private float effectTime;
+
   public float Radius { get { return transform.lossyScale.x; } }
 
   //For use only in BallUserComponent.HoldBall
   public void SetPosition(Vector3 newPosition) {
+    DigitalRuby.LightningBolt.LightningBoltScript gfx = Instantiate(lightningEffectPrefab);
+    gfx.StartPosition = transform.position;
     transform.localPosition = newPosition;
+    gfx.EndPosition = transform.position;
+    StartCoroutine(GfxRoutine(gfx.gameObject));
   }
 
   //For use only in BallUserComponent.HoldBall
   public void SetParent(Transform newParent) {
     transform.SetParent(newParent, true);
+  }
+
+  private IEnumerator GfxRoutine(GameObject effect) {
+    yield return new WaitForSeconds(effectTime);
+    Destroy(effect);
   }
 }
