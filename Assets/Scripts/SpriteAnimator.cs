4211ef65033230a8108b1bd81dbe2f09c861876c
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-14 23:17:44 -0500

    Add SpriteAnimator script

---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour {
+#pragma warning disable 0649
+  [SerializeField] private List<Sprite> sprites;
+#pragma warning restore 0649
+
+  private SpriteRenderer spriteRenderer;
+
+  private void Awake() {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    StartCoroutine(AnimationRoutine());
+  }
+
+  private IEnumerator AnimationRoutine() {
+    while (true) {
+      foreach (Sprite sprite in sprites) {
+        spriteRenderer.sprite = sprite;
+        yield return null;
+      }
+    }
+  }
+}
