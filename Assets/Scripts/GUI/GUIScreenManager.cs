e88968555e3d8efbb582ac008190dc5310549463
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-17 11:18:36 -0500

    Add ScreenManager for GUI

---
+++ Assets/Scripts/GUI/GUIScreenManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIScreenManager : MonoBehaviour {
+  [SerializeField] private GameObject mainMenu;
+  [SerializeField] private GameObject playerSelection;
+  [SerializeField] private GameObject mainGame;
+}
