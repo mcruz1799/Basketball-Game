f2c5326a5dfd3245c4fd228223e7d8ee0c9cd463
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-17 19:31:44 -0500

    Add and implement a FollowTransform script

---
+++ Assets/Scripts/FollowTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour {
+#pragma warning disable 0649
+  [SerializeField] private Transform toFollow;
+  [SerializeField] private Vector3 localOffset;
+  [SerializeField] private bool useInspectorOffset;
+#pragma warning restore 0649
+
+  private void Awake() {
+    if (useInspectorOffset) {
+      localOffset = transform.position - toFollow.position;
+    }
+  }
+
+  private void Update() {
+    transform.position = toFollow.position + localOffset;
+  }
+}
