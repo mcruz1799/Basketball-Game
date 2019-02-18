b715bd78ee6e9d10998e8687174debbe4e730828
ShadyPastry <hwoodrow@andrew.cmu.edu>
2019-02-18 11:34:43 -0500

    Change ball animation

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour, IBall {
 #pragma warning disable 0649
-  [SerializeField] private DigitalRuby.LightningBolt.LightningBoltScript lightningEffectPrefab;
-  [SerializeField] private float effectTime;
+  [Range(0.1f, 10f)] [SerializeField] private float moveAnimationSpeed;
 #pragma warning restore 0649
+
+  private Vector3? targetLocalPosition;
 
   public float Radius { get { return transform.lossyScale.x; } }
 
+  private void Awake() {
+    StartCoroutine(GfxRoutine());
+  }
+
   //For use only in BallUserComponent.HoldBall
-  public void SetPosition(Vector3 newPosition) {
-    DigitalRuby.LightningBolt.LightningBoltScript gfx = Instantiate(lightningEffectPrefab);
-    gfx.StartPosition = transform.position;
-    transform.localPosition = newPosition;
-    gfx.EndPosition = transform.position;
-    StartCoroutine(GfxRoutine(gfx.gameObject));
+  public void SetPosition(Vector3 newLocalPosition) {
+    targetLocalPosition = newLocalPosition;
+    StartCoroutine(GfxRoutine());
   }
 
   //For use only in BallUserComponent.HoldBall
@@ -24,8 +26,19 @@ public class Ball : MonoBehaviour, IBall {
     transform.SetParent(newParent, true);
   }
 
-  private IEnumerator GfxRoutine(GameObject effect) {
-    yield return new WaitForSeconds(effectTime);
-    Destroy(effect);
+  private IEnumerator GfxRoutine() {
+    while (true) {
+      if (targetLocalPosition == null) {
+        yield return new WaitUntil(() => targetLocalPosition.HasValue);
+      }
+
+      Vector3 selfToTarget = (targetLocalPosition.Value - transform.localPosition);
+      if (selfToTarget.sqrMagnitude < 0.04f) {
+        transform.localPosition = targetLocalPosition.Value;
+      } else {
+        transform.localPosition += moveAnimationSpeed * Time.deltaTime * selfToTarget.normalized;
+      }
+      yield return null;
+    }
   }
 }
