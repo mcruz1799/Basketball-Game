32e8942f0ebc6469adcdf355331e1f61e4934d74
Randy D. Epouhe <repouhe@andrew.cmu.edu>
2019-02-21 19:02:03 -0500

    Uncommented GUI lines.

--- Assets/Scripts/GUI/PlayerSelectionGUI.cs
+++ Assets/Scripts/GUI/PlayerSelectionGUI.cs
@@ -26,6 +26,8 @@ public class PlayerSelectionGUI : MonoBehaviour {
   [SerializeField] private RawImage controller3Ball;
   [SerializeField] private RawImage controller4X;
   [SerializeField] private RawImage controller4Ball;
+
+  [SerializeField] private AudioClip selectSound;
   
 #pragma warning restore 0649
 
@@ -52,65 +54,65 @@ public class PlayerSelectionGUI : MonoBehaviour {
       switch (info.Key) {
         case XboxController.First:
           controller1PlayerImage.sprite = sprite;
-          // if (isConfirmed) 
-          // {
-          //   controller1Ball.gameObject.SetActive(true);
-          //   controller1X.gameObject.SetActive(false);
-          //   SoundManager.Instance.Play(selectSound);
-          // }
-          // else
-          // {
-          //   controller1Ball.gameObject.SetActive(false);
-          //   controller1X.gameObject.SetActive(true);
-          // }
+          if (isConfirmed) 
+          {
+             controller1Ball.gameObject.SetActive(true);
+             controller1X.gameObject.SetActive(false);
+             SoundManager.Instance.Play(selectSound);
+          }
+          else
+          {
+             controller1Ball.gameObject.SetActive(false);
+             controller1X.gameObject.SetActive(true);
+          }
           controller1Prompt.color = textColor;
           controller1Prompt.text = text;
           break;
         case XboxController.Second:
           controller2PlayerImage.sprite = sprite;
-          // if (isConfirmed) 
-          // {
-          //   controller2Ball.gameObject.SetActive(true);
-          //   controller2X.gameObject.SetActive(false);
-          //   SoundManager.Instance.Play(selectSound);
-          // }
-          // else
-          // {
-          //   controller2Ball.gameObject.SetActive(false);
-          //   controller2X.gameObject.SetActive(true);
-          // }
+          if (isConfirmed) 
+          {
+             controller2Ball.gameObject.SetActive(true);
+             controller2X.gameObject.SetActive(false);
+             SoundManager.Instance.Play(selectSound);
+          }
+          else
+          {
+             controller2Ball.gameObject.SetActive(false);
+             controller2X.gameObject.SetActive(true);
+          }
           controller2Prompt.color = textColor;
           controller2Prompt.text = text;
           break;
         case XboxController.Third:
           controller3PlayerImage.sprite = sprite;
-          // if (isConfirmed) 
-          // {
-          //   controller3Ball.gameObject.SetActive(true);
-          //   controller3X.gameObject.SetActive(false);
-          //   SoundManager.Instance.Play(selectSound);
-          // }
-          // else
-          // {
-          //   controller3Ball.gameObject.SetActive(false);
-          //   controller3X.gameObject.SetActive(true);
-          // }
+          if (isConfirmed) 
+          {
+             controller3Ball.gameObject.SetActive(true);
+             controller3X.gameObject.SetActive(false);
+             SoundManager.Instance.Play(selectSound);
+          }
+          else
+          {
+             controller3Ball.gameObject.SetActive(false);
+             controller3X.gameObject.SetActive(true);
+          }
           controller3Prompt.color = textColor;
           controller3Prompt.text = text;
           break;
         case XboxController.Fourth:
           controller4PlayerImage.sprite = sprite;
-          // if (isConfirmed) 
-          // {
-          //   controller4Ball.gameObject.SetActive(true);
-          //   controller4X.gameObject.SetActive(false);
-          //   SoundManager.Instance.Play(selectSound);
-          // }
-          // else
-          // {
-          //   controller4Ball.gameObject.SetActive(false);
-          //   controller4X.gameObject.SetActive(true);
-          // }
+          if (isConfirmed) 
+          {
+             controller4Ball.gameObject.SetActive(true);
+             controller4X.gameObject.SetActive(false);
+             SoundManager.Instance.Play(selectSound);
+          }
+          else
+          {
+             controller4Ball.gameObject.SetActive(false);
+             controller4X.gameObject.SetActive(true);
+          }
           controller4Prompt.color = textColor;
           controller4Prompt.text = text;
           break;
